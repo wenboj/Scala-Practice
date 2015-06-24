46ad88941c6c0a5100ca9dff013fbc8fa22476fd
wenboj <wilburgca@gmail.com>
2015-06-24 15:21:52 -0700

    Modify HelloWorld

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -2,5 +2,8 @@
  * Created by wjiang on 24/06/2015.
  */
 object HelloWorld {
-  def main(args:Array[String]): Unit = println("Hello World!")
+  def main(args:Array[String]): Unit = {
+    println("Hello! World!")
+    println("Haha")
+  }
 }
